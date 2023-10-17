7e3cd14c1ac6d38cc187849903e5373a48cfef28
Juan C Garcia <juancgarcia@Juans-MacBook-Air.local>
2023-10-16 22:19:50 -0400

    assets folder restore

---
+++ src/assets/index.js
@@ -0,0 +1,20 @@
+import me from "./images/bannerImg.png";
+import projectOne from "./images/projects/WeatherDashboard.jpg";
+import projectTwo from "./images/projects/TechBlog.jpg";
+import projectThree from "./images/projects/WorkDayScheduler.jpg";
+import projectFour from "./images/projects/PasswordGenerator.jpg";
+import projectFive from "./images/projects/NoteTaker.jpg";
+import projectSix from "./images/projects/MyWorkoutPal.jpg";
+import resume from "./images/projects/JuanCruzGarciaResume.pdf";
+
+
+export {
+  me,
+  projectOne,
+  projectTwo,
+  projectThree,
+  projectFour,
+  projectFive,
+  projectSix,
+  resume
+};
